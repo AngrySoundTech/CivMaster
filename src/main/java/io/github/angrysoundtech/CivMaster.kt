eef9de64e3d0f310fb95bee782f39226d9a790da
AngrySoundTech <angrysoundtech@gmail.com>
2020-06-21 12:34:24 -0400

    Customise template

---
+++ src/main/java/io/github/angrysoundtech/CivMaster.kt
@@ -0,0 +1,34 @@
+/**
+ * Copyright (C) 2020 AngrySoundTech
+ * This file is part of CivMaster.
+ *
+ * CivMaster is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CivMaster is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CivMaster.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package io.github.angrysoundtech
+
+import net.minecraftforge.fml.common.Mod
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+
+const val MOD_ID = "civmaster"
+
+@Mod(MOD_ID)
+object CivMaster {
+
+    val logger: Logger = LogManager.getLogger(MOD_ID)
+
+    init {
+
+    }
+}
